1dd472613a9181a400e8e8d3a5518f3bae369a1b
midorikocak <mtkocak@gmail.com>
2018-10-20 06:32:13 +0900

    changes

--- mail.php
+++ mail.php
@@ -25,13 +25,14 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'mtkocak@gmail.com';                 // SMTP username
-$mail->Password = 'W38N41f1';                           // SMTP password
+$mail->Password = 'xmA1e#ST';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
 $mail->From = $email;
-$mail->FromName = 'Mailer';
-$mail->addAddress($email, 'Mutlu Kocak');     // Add a recipient
+$mail->FromName = $email;
+$mail->addAddress('mtkocak@gmail.com', 'Mutlu Kocak');     // Add a recipient
+$mail->AddReplyTo($email, $email);
 
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 $mail->isHTML(true);                                  // Set email format to HTML
